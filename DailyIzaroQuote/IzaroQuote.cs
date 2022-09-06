291f90e2e4a1709e095439bba841fab502389ea5
lbischoff <lbischoff@systhemis.de>
2022-09-06 15:24:36 +0200

    Projektdateien hinzufügen.

---
+++ DailyIzaroQuote/IzaroQuote.cs
@@ -0,0 +1,32 @@
+﻿using DailyIzaroQuote.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyIzaroQuote
+{
+    //writes random random Izaro Quote from txt ressource file to console
+    internal class IzaroQuote
+    {
+        //txt file as resource string in project, attribute of class IzaroQuote
+        public string izaro = Resources.IzarosQuotes;
+
+        public string RandomQuote()
+        {
+            //split resource string each line as element in an array of strings             
+            string[] lines = izaro.Split(Environment.NewLine.ToCharArray());
+            int lineCount = lines.Length;
+
+            Random random = new Random();
+            int lineNum = random.Next(lineCount);
+
+            string result = "Izaro: " + lines[lineNum];
+
+            return result;
+
+        }
+    }
+}
---
+++ DailyIzaroQuote/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DailyIzaroQuote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IzaroQuote izaroquote = new IzaroQuote();
+            Console.WriteLine(izaroquote.RandomQuote());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
